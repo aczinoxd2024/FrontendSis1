7e12e5411528368cf05055eb5516e9541faeb7c3
Roly <lijeronhr10@gmail.com>
2025-05-01 16:52:04 -0400

    PreLogin

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-// frontend: main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()) // <-- Soporte para interceptores
   ]
 }).catch(err => console.error(err));
+
